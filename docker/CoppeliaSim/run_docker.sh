5763290dfc4e79d6c5ccbaa61c89b227c3d1d9a9
Wei <21wjing@gmail.com>
2024-09-17 21:42:34 +0800

    add rvt docker file

--- docker/CoppeliaSim/run_docker.sh
+++ docker/CoppeliaSim/run_docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 定义容器名称
-CONTAINER_NAME="rlbench_v1"
+CONTAINER_NAME="CoppeliaSim_v1"
 
-# 定义Docker镜像名称
-# IMAGE_NAME="pytorch/pytorch:1.12.1-cuda11.3-cudnn8-runtime"
-IMAGE_NAME="rlbench:v0.1"
+# 定义Docker镜像名称, change the name to the docker image name
+# suggest using docker tag to rename
+IMAGE_NAME="xxxx"
 # 检查是否已经有同名容器在运行
 if [ $(docker ps -aq -f name=^/${CONTAINER_NAME}$ | wc -l) -ne 0 ]; then
     echo "容器已存在，正在停止并删除旧容器..."
---
+++ docker/Docker_RVT/README.md
@@ -0,0 +1,6 @@
+# Build docker container for RVT
+
+clone the RVT repo and its submodules
+ ` git clone --recurse-submodules git@github.com:NVlabs/RVT.git && cd RVT && git submodule update --init 
+ 
+ docker build .
